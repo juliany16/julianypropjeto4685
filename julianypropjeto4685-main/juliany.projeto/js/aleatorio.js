970918512a7b86217abef8552a9148f42459a369
juliany16 <127852045+juliany16@users.noreply.github.com>
2024-09-18 09:12:42 -0300

    Delete julianypropjeto4685-main directory

--- julianypropjeto4685-main/juliany.projeto/js/aleatorio.js
+++
@@ -1,7 +0,0 @@
-const nomes = ["Otávio", "Nathan", "Maria Eloísa", "Gabriel", "Ana", "Sinclair", "Demian"];
-export function aleatorio (lista){
-    const posicao = Math.floor(Math.random()* lista.length);
-    return lista[posicao];
-}
-
-export const nome = aleatorio(nomes)
